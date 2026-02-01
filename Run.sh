2c354ff58ee3ebe3614a2e3688c5abf66372176a
StarinskyO <oleg.starynski@gmail.com>
2026-02-01 22:21:03 +0300

    Добавил Тест B00

--- Run.sh
+++ Run.sh
@@ -10,9 +10,11 @@
 #build/apps/Test_A/A05/test_A05
 #build/apps/Test_A/A06/test_A06
 #build/apps/Test_A/A07/test_A07
-build/apps/Test_A/A08/test_A08
+#build/apps/Test_A/A08/test_A08
 #build/apps/Test_A/A09/test_A09
 #build/apps/Test_A/A10/test_A10
 
+build/apps/Test_B/B00/test_B00
+#build/apps/Test_B/B01/test_B01
 
 #build/apps/snake/snake
